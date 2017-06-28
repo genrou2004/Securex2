16c2d33059c8729166b66aa64f3567b87d88ce41
genrou2004 <student@GBTC440J10LX.montgomerycollege.edu>
2017-06-28 13:02:11 -0400

    Added custom login page

--- src/main/java/byAJ/Securex/configs/SecurityConfiguration.java
+++ src/main/java/byAJ/Securex/configs/SecurityConfiguration.java
@@ -18,7 +18,11 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter{
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-                .authorizeRequests().anyRequest().authenticated();
+                .authorizeRequests()
+                    .anyRequest().authenticated()
+                    .and()
+                .formLogin()
+                    .loginPage("/login");
         http
                 .formLogin().failureUrl("/login?error")
                 .defaultSuccessUrl("/")
